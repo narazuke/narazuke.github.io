a94789e1bdff7ee9af5fadfef8ae1c3f6736ce70
wabetarou <watanabe.a.aq@m.titech.ac.jp>
2022-06-25 23:52:16 +0900

    キャッシュ削除

--- .github/actions/export-secret/index.js
+++
@@ -1,8 +0,0 @@
-const secrets = {
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  NARAZUKE_ID: process.env.NARAZUKE_CLIENT_ID,
-  NARAZUKE_SECRET: process.env.NARAZUKE_CLIENT_SECRET,
-  PROXY: process.env.PROXY
-}
-
-module.exports = secrets
